3f06ae9ae65842a405044d9e9008d7e3bb945c5f
Biniyam Girma <biniswengineer@gmail.com>
2025-09-07 22:58:52 +0300

    add

---
+++ django-models/LibraryProject/relationship_app/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=150)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    
+    class Meta:
+        permissions = [('can_add_book', 'can add book'),('can_change_book', 'can change book'), ('can_delete_book', 'can delete book')]
+    
+    def __str__(self):
+        return self.title
+
+class Library(models.Model):
+    name = models.CharField(max_length=150)
+    books = models.ManyToManyField(Book)
+
+    def __str__(self):
+        return self.name
+
+class Librarian(models.Model):
+    name = models.CharField(max_length=150)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=20,
+                            default='Member',
+                            choices=[('Admin', 'Admin'), ('Librarian', 'Librarian'), ('Member', 'Member')])
